71f6c1e0bd9ff9ef34367cf58e3438cb496c846f
yangzec <yangzec@gmail.com>
2024-11-12 12:22:15 +0800

    1

--- themes/starter/components/About.js
+++ themes/starter/components/About.js
@@ -59,7 +59,7 @@ export const About = () => {
 
                     <div className='relative z-10 mb-4 flex items-center justify-center overflow-hidden bg-primary px-6 py-12 sm:mb-8 sm:h-[160px] sm:p-5 lg:mb-4 xl:mb-8'>
                       <div>
-                        <span className='block text-5xl font-extrabold text-white'>
+                        <span className='block text-3xl font-extrabold text-white'>
                           {siteConfig('STARTER_ABOUT_TIPS_1')}
                         </span>
                         <span className='block text-base font-semibold text-white'>
--- themes/starter/components/Features.js
+++ themes/starter/components/Features.js
@@ -15,7 +15,7 @@ export const Features = () => {
         <div className='container'>
           <div className='-mx-4 flex flex-wrap'>
             <div className='w-full px-4'>
-              <div className='mx-auto mb-12 max-w-[485px] text-center lg:mb-[70px]'>
+              <div className='mx-auto mb-12 max-w-[685px] text-center lg:mb-[70px]'>
                 <span className='mb-2 block text-lg font-semibold text-primary'>
                   {siteConfig('STARTER_FEATURE_TITLE')}
                 </span>
