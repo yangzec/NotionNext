81a903e5dc2ec0bbe870e4e8feaaffd9ef673d26
yangzec <yangzec@gmail.com>
2024-11-13 11:43:00 +0800

    Update Brand.js

--- themes/starter/components/Brand.js
+++ themes/starter/components/Brand.js
@@ -18,7 +18,7 @@ export const Brand = () => {
             <div className='w-full px-4'>
               <div className='mx-auto mb-12 max-w-[485px] text-center lg:mb-[70px]'>
                 <span className='mb-2 block text-lg font-semibold text-primary'>
-                  {siteConfig('STARTER_BRAND_TITLE', null, CONFIG)}
+                  {siteConfig('STARTER_BRAND_TITLE')}
                   {/* Our Partners */}
                 </span>
                 <h2 className='mb-3 text-3xl font-bold text-dark dark:text-white sm:text-4xl md:text-[40px] md:leading-[1.2]'>
