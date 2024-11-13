623a1f2a63d8b178595d3fe207c80ed5bdb0008a
yangzec <yangzec@gmail.com>
2024-11-13 12:01:18 +0800

    1

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -242,12 +242,12 @@ const CONFIG = {
 
   // 联系模块
   STARTER_CONTACT_ENABLE: true, // 联系模块开关
-  STARTER_CONTACT_TITLE: '联系我们',
-  STARTER_CONTACT_TEXT: '告诉我们您遇到的问题',
-  STARTER_CONTACT_LOCATION_TITLE: '我们的位置',
-  STARTER_CONTACT_LOCATION_TEXT: '中国，福建',
-  STARTER_CONTACT_EMAIL_TITLE: '我们如何帮助您？',
-  STARTER_CONTACT_EMAIL_TEXT: 'mail@tangly1024.com',
+  STARTER_CONTACT_TITLE: 'Contact Us',
+  STARTER_CONTACT_TEXT: 'Tell us what you need',
+  STARTER_CONTACT_LOCATION_TITLE: 'Location',
+  STARTER_CONTACT_LOCATION_TEXT: 'Fujian, China',
+  STARTER_CONTACT_EMAIL_TITLE: 'Get a quote',
+  STARTER_CONTACT_EMAIL_TEXT: 'quote@superfactory.net',
 
   // 嵌入外部表单
   STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // 基于NoteForm创建，将留言数据存在Notion中
