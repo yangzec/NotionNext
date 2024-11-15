0a601539d239ae4986f0683e68410c2522613a53
yangzec <yangzec@gmail.com>
2024-11-15 11:55:21 +0800

    1

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'tcgpocketpokemon', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Pokemon Tcg Pocket', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tcgpocketpokemon.com/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tcgpocketpokemon.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
