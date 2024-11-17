e880c22e72dc1701dcff832895c7c2c5accc4811
yangzec <yangzec@gmail.com>
2024-11-18 01:02:37 +0800

    Update Footer.js

--- themes/game/components/Footer.js
+++ themes/game/components/Footer.js
@@ -19,13 +19,13 @@ export const Footer = props => {
         <p>{siteConfig('DESCRIPTION')}</p>
 
         <span className='dark:text-gray-200 no-underline ml-4'>
-          Powered by
+          {/* Powered by
           <a
             href='https://github.com/tangly1024/NotionNext'
             className=' hover:underline'>
             {' '}
             NotionNext {siteConfig('VERSION')}{' '}
-          </a>
+          </a> */}
         </span>
       </div>
     </footer>
