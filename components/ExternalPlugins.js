9db08978958b80894f45353a228842c38d8c8021
yangzec <yangzec@gmail.com>
2024-11-20 01:32:50 +0800

    Update ExternalPlugins.js

--- components/ExternalPlugins.js
+++ components/ExternalPlugins.js
@@ -366,10 +366,11 @@ const ExternalPlugin = props => {
 
       {/* Plausible 统计 */}
       {LINK && (
-        <script defer data-domain={LINK} src="https://plausible.io/js/script.js"></script>
+        <script defer data-domain={LINK} src="https://gshqeklb.sealosbja.site/js/script.js"></script>
         // <script defer data-domain="sprunkincredibox.online" src="https://plausible.io/js/script.js"></script>
         // <script defer data-domain="n.sprunkincredibox.online" src="https://plausible.io/js/script.js"></script>
       )}
+    <script defer data-domain="tcgpocketpokemon.com" src="https://plausible.io/js/script.js"></script>
     </>
   )
 }
