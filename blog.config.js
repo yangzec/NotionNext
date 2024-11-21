c08f0f98303b7686f56c5e2d5c8603d600ffab58
yangzec <yangzec@gmail.com>
2024-11-21 23:10:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-    '14550e4b0a3581b0bb90df9312427446',
+    '14550e4b0a3581b0bb90df9312427446,fr:14550e4b0a358081ab11e344436f94f2,ko:14550e4b0a3580759dc6c381b27feb21,jp:14550e4b0a3580ee95fef2203717372f,it:14550e4b0a35804c9626e3d97c4794ff,es:14550e4b0a35805490ead3935c153c5a,',
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || false,
   PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
   NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 5, // 更新内容缓存间隔 单位(秒)；即每个页面有5秒的纯静态期、此期间无论多少次访问都不会抓取notion数据；调大该值有助于节省Vercel资源、同时提升访问速率，但也会使文章更新有延迟。
